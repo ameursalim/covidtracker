1b4176f2574a0375be6258c38f2720605e7a665a
salim ameur <salim.ameur@gmail.com>
2021-02-25 18:31:59 +0100

    styling

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  const dateObj = new Date();
+  const currentDate = dateObj.getFullYear();
+  return (
+    <footer>
+      <p className="text-gray-500 w-full text-center mt-10">
+        {" "}
+        copyright {currentDate}{" "}
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
