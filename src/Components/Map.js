35c158ff65592a5455ce9aef7793130c812caeb3
salim ameur <salim.ameur@gmail.com>
2021-02-25 18:57:39 +0100

    updating map

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -3,10 +3,14 @@ import { MapContainer, TileLayer } from "react-leaflet";
 import { showDataOnMap } from "../utils";
 function Map({ countries, casesType, center, zoom }) {
   return (
-    <div className="h-2/3 rounded-lg  bg-gray-800 p-3">
-      <MapContainer center={center} zoom={zoom} className="h-full  w-full">
+    <div className="h-2/3 rounded-lg  p-4">
+      <MapContainer
+        center={center}
+        zoom={zoom}
+        className="h-full  w-full rounded-lg p-4"
+      >
         <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://api.mapbox.com/styles/v1/salimameur/ckky7y6qx19fk17od79pe7y1e/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2FsaW1hbWV1ciIsImEiOiJja2t5N3JleGkybndzMm9xbmZ6cXA3NXFtIn0.i8b6gAuKf9ZdhG8cosM0PA"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
         {showDataOnMap(countries, casesType)}
